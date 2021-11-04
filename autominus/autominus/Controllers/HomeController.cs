d922be0ab34907a75a8b81422897671e8f0d527e
Laucek <43381869+Unholy57@users.noreply.github.com>
2021-11-04 18:41:07 +0200

    Ai kazkas

--- autominus/autominus/Controllers/HomeController.cs
+++ autominus/autominus/Controllers/HomeController.cs
@@ -22,6 +22,18 @@ namespace autominus.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
