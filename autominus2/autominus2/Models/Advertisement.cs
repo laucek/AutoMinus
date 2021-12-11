eaaee50e403761391cb48086eb0e6758a19df645
AidasV1 <aidasv523@gmail.com>
2021-12-11 18:59:04 +0200

    Enter a message <Required>

---
+++ autominus2/autominus2/Models/Advertisement.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace autominus2.Models
+{
+    public class Advertisement
+    {
+        public DateTime AdCreationDate { get; set; }
+        public string FuelType { get; set; }
+        public int Mileage { get; set; }
+        public string Vin { get; set; }
+        public string EngineCapacity { get; set; }
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public int DoorCount { get; set; }
+        public DateTime ModelYear { get; set; }
+        public float Price { get; set; }
+        public int Drivetrain { get; set; }
+        public string Power { get; set; }
+        public string Damage { get; set; }
+        public string Color { get; set; }
+        public int SeatCount { get; set; }
+        public string WheelPosition { get; set; }
+        public string FirstRegistrationCountry { get; set; }
+        public string Co2Emissions { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Gearbox { get; set; }
+        public string BodyType { get; set; }
+
+        public Advertisement(DateTime adCreationDate, string fuelType, int mileage, string vin, string engineCapacity,
+            string model, string make, int doorCount, DateTime modelYear, float price, int drivetrain, string power, string damage,
+            string color, int seatCount, string wheelPosition, string firstRegistrationCountry, string co2Emissions, string city,
+            string country, string phoneNumber, string gearbox, string bodyType)
+        {
+            AdCreationDate = adCreationDate;
+            FuelType = fuelType;
+            Mileage = mileage;
+            Vin = vin;
+            EngineCapacity = engineCapacity;
+            Model = model;
+            Make = make;
+            DoorCount = doorCount;
+            ModelYear = modelYear;
+            Price = price;
+            Drivetrain = drivetrain;
+            Power = power;
+            Damage = damage;
+            Color = color;
+            SeatCount = seatCount;
+            WheelPosition = wheelPosition;
+            FirstRegistrationCountry = firstRegistrationCountry;
+            Co2Emissions = co2Emissions;
+            City = city;
+            Country = country;
+            PhoneNumber = phoneNumber;
+            Gearbox = gearbox;
+            BodyType = bodyType;
+        }
+    }
+}
--- autominus2/autominus2/Utils/CarsRepo.cs
+++ autominus2/autominus2/Utils/CarsRepo.cs
@@ -2,10 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using autominus2.Models;
+using MySql.Data.MySqlClient;
 
 namespace autominus2.Utils
 {
     public class CarsRepo
     {
+        //public void InsertAdvertisement()
+        //{
+        //    string sql = "INSERT INTO `Skelbimas`(`skelbimo_sukurimo_data`, `kuro_tipas`, `rida`, `	vin_kodas`, `darbinis_turis`," +
+        //        " `modelis`, `marke`, `duru_skaicius`, `metai`, `kaina`, `varantieji_ratai`, `galia`, `defektai`, `spalva`," +
+        //        " `sedimu_vietu_skaicius`, `vairo_padetis`, `pirmosios_registracijos_salis`, `co2_emisija`, `miestas`, `salis`, " +
+        //        " `telefono_numeris`, `pavaru_deze`, `kebulo_tipas`)" +
+        //        " VALUES (NOW(), 'kuras', 10000, 'WBA48941321', '2999', 'modelis', 'marke', 4, NOW(), 2599, 2, '180', 'defektai', 'spalva'" +
+        //        " 5, 'vairas', 'psalis', '190', 'miestas', 'salis', '+370804', 'pavarudeze', 'kebulas')";
+        //}
+        public static bool InsertAdvertisement(Advertisement ad)
+        {
+            try
+            {
+                string sql = "INSERT INTO `Skelbimas`(`skelbimo_sukurimo_data`, `kuro_tipas`, `rida`, `	vin_kodas`, `darbinis_turis`," +
+                " `modelis`, `marke`, `duru_skaicius`, `metai`, `kaina`, `varantieji_ratai`, `galia`, `defektai`, `spalva`," +
+                " `sedimu_vietu_skaicius`, `vairo_padetis`, `pirmosios_registracijos_salis`, `co2_emisija`, `miestas`, `salis`, " +
+                " `telefono_numeris`, `pavaru_deze`, `kebulo_tipas`)" +
+                $" VALUES (NOW(), 'kuras', 10000, 'WBA48941321', '2999', 'modelis', 'marke', 4, NOW(), 2599, 2, '180', 'defektai', 'spalva'" +
+                $" 5, 'vairas', 'psalis', '190', 'miestas', 'salis', '+370804', 'pavarudeze', 'kebulas')";
+
+                string conn = "server=sql11.freemysqlhosting.net;port=3306;database=sql11458082;user=sql11458082;password=2dEuRL4y8A";
+                MySqlConnection mySqlConnection = new MySqlConnection(conn);
+                MySqlCommand mySqlCommand = new MySqlCommand(sql, mySqlConnection);
+                mySqlConnection.Open();
+                int temp = mySqlCommand.ExecuteNonQuery();
+                mySqlConnection.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
     }
 }
